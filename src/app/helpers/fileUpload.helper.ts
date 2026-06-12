778e9d35ef2e3a6cc2443808ed997fce00f244cd
undefinedSahed <sahedrahmanshakil@gmail.com>
2026-06-13 03:12:05 +0600

    feat: added workflow file for github action

---
+++ src/app/helpers/fileUpload.helper.ts
@@ -0,0 +1,127 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { ENV } from '@src/env';
+import { mimeTypeMapping } from '@src/shared/constants/mimeTypes.constants';
+import { firstValueFrom } from 'rxjs';
+import { Readable } from 'stream';
+
+@Injectable()
+export class FileUploadHelper {
+  constructor(private readonly http: HttpService) {
+    this.s3 = new S3Client({
+      region: 'auto',
+      endpoint: `https://${ENV.s3.endpoint}`,
+      credentials: {
+        accessKeyId: ENV.s3.accessKey,
+        secretAccessKey: ENV.s3.secretKey,
+      },
+    });
+  }
+  private s3: S3Client;
+
+  public async uploadBinary(
+    folder = 'media',
+    binary: Buffer | Readable,
+    fileName?: string,
+    contentType?: string,
+  ): Promise<string> {
+    try {
+      const key = fileName || `${Date.now()}`;
+      const command = new PutObjectCommand({
+        Bucket: ENV.s3.bucket,
+        Key: `${ENV.s3.folderPrefix}/${folder}/${key}`,
+        Body: binary,
+        ContentType: contentType || 'image/jpeg',
+        ACL: 'public-read',
+      });
+
+      await this.s3.send(command);
+
+      const endpoint = ENV.s3.endpoint?.replace(/\/$/, '');
+      const url = `https://${endpoint}/${ENV.s3.bucket}/${ENV.s3.folderPrefix}/${folder}/${key}`;
+      return url;
+    } catch (error) {
+      console.error('🚀 ~ FileUploadHelper ~ error:', error);
+      return '';
+    }
+  }
+
+  public async downloadAndUploadToSpace(payload: {
+    fileUrl: string;
+    accessToken?: string;
+    fileName?: string;
+  }): Promise<string> {
+    try {
+      const mediaResponse = await this.http.get(payload.fileUrl, {
+        headers: {
+          Authorization: `Bearer ${payload.accessToken}`,
+        },
+        responseType: 'arraybuffer',
+      });
+
+      const mediaData = await firstValueFrom(mediaResponse);
+      const contentType = mediaData.headers['content-type'];
+      const extension = mimeTypeMapping[contentType] || 'jpg';
+      const s3Key = `${payload.fileName}.${extension}` || `${Date.now()}.${extension}`;
+      const binary = mediaData.data;
+
+      return this.uploadBinary('media-manager', binary, s3Key, contentType);
+    } catch (error) {
+      console.error('🚀 ~ FileUploadHelper ~ error:', error);
+      return '';
+    }
+  }
+
+  public async downloadAndUploadToSpaceV2(payload: {
+    fileUrl: string;
+    accessToken?: string;
+    fileName?: string;
+  }): Promise<string> {
+    try {
+      const mediaResponse = await this.http.get(payload.fileUrl, {
+        headers: {
+          Authorization: `Bearer ${payload.accessToken}`,
+        },
+        responseType: 'arraybuffer',
+      });
+
+      const mediaData = await firstValueFrom(mediaResponse);
+      const contentType = mediaData.headers['content-type'];
+      const extension = mimeTypeMapping[contentType] || 'jpg';
+      const s3Key = `${payload.fileName}.${extension}` || `${Date.now()}.${extension}`;
+      const binary = mediaData.data;
+
+      return this.uploadBinary('media-manager-v2', binary, s3Key, contentType);
+    } catch (error) {
+      console.error('🚀 ~ FileUploadHelper ~ error:', error);
+      return '';
+    }
+  }
+
+  public async downloadAndUploadToSpaceTelegram(payload: {
+    fileUrl: string;
+    accessToken?: string;
+    fileName?: string;
+  }): Promise<string> {
+    try {
+      const mediaResponse = await this.http.get(payload.fileUrl, {
+        headers: {
+          Authorization: `Bearer ${payload.accessToken}`,
+        },
+        responseType: 'arraybuffer',
+      });
+
+      const mediaData = await firstValueFrom(mediaResponse);
+      const contentType = mediaData.headers['content-type'];
+      const extension = mimeTypeMapping[contentType] || 'jpg';
+      const s3Key = `${payload.fileName}.${extension}` || `${Date.now()}.${extension}`;
+      const binary = mediaData.data;
+
+      return this.uploadBinary('telegram', binary, s3Key, contentType);
+    } catch (error) {
+      console.error('🚀 ~ FileUploadHelper ~ error:', error);
+      return '';
+    }
+  }
+}
